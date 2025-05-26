e80af8053102005ce04ccd291d0b8e53fc68c83b
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-05-26 16:09:13 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -94,9 +94,9 @@ else
    echo ""
 fi
 
-if [ $OSTYPE != "DreamOs" ]; then
-	opkg update && opkg --force-reinstall --force-overwrite install ffmpeg gstplayer exteplayer3 enigma2-plugin-systemplugins-serviceapp
-fi
+# if [ $OSTYPE != "DreamOs" ]; then
+	# opkg update && opkg --force-reinstall --force-overwrite install ffmpeg gstplayer exteplayer3 enigma2-plugin-systemplugins-serviceapp
+# fi
 sleep 2
 
 wget --no-check-certificate 'https://github.com/Belfagor2005/Archimede-M3UConverter/archive/refs/heads/main.tar.gz'
